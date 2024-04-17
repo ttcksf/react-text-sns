edbd62532ed9c60486cad4fbf7a42b4505b00c7c
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-04-17 15:56:49 +0900

    create:App.js index.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bgBlur bgBlurLeftTop"></div>
+      <div className="bgBlur bgBlurRightTop"></div>
+      <div className="bgBlur bgBlurLeftBottom"></div>
+      <div className="bgBlur bgBlurRightBottom"></div>
     </div>
   );
 }
